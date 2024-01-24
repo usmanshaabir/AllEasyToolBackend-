7958c1ad138e95b4493f4d2773a0ed6dc9c30c09
Nauman Shabbir <mnaumanshabbir@gmail.com>
2024-01-24 16:05:38 +0500

    Post Api Created successfully!

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-// const mongoose = require("mongoose");
-// const http = require("http");
+const mongoose = require("mongoose");
+
+
+const userRoutes = require("./src/Routes/UserRoutes");
+
 
 require("dotenv").config()
 
@@ -13,13 +16,30 @@ app.use(cors())
 app.use(express.json())
 
 // Routes
+app.use("/", userRoutes);
 
-app.get("/", async (req, res) => {
-    res.send("hello word I m nauman with my gf ")
+// monogoose Conection
+const dbUrl = process.env.CONNECTION_STRING
 
-});
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log(`Connected to MongoDB: ${dbUrl}`);
+}).catch((error) => {
+    console.error(`MongoDB connection error: ${error}`);
+})
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+const db = mongoose.connection;
+
+db.on("error", (error) => {
+    console.error('MongoDB connection error:', error);
+})
+db.once("open", () => {
+    console.log('Connected to MongoDB');
+    const PORT = process.env.PORT
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    })
+
+})
